5d3848bedc3fa39e82f6413e81b55a5982131fda
klaradiedrichs <klara.diedrichs@study.hs-duesseldorf.de>
2023-11-16 18:53:06 +0100

    Wechsel zwischen Overview und Road View mit zwei neuen useStates

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, Stage, Float, Text, ContactShadows} from '@react-three/drei'
+import { Environment, Stage, Float, Text, ContactShadows, TransformControls} from '@react-three/drei'
 import React, { useState, useEffect, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import Player from './Player'
@@ -6,11 +6,15 @@ import Player from './Player'
   
 export default function Experience()
 {
+    const textRef = useRef();
 
     const [newRotation, setNewRotation] = useState(null);
 
     const handleMeshClick = (e) => {
         setNewRotation([0, 0.2, 0]);
+        console.log("Text Rotation X" , textRef.current.rotation.x)
+        console.log("Text Rotation Y" , textRef.current.rotation.y)
+        console.log("Text Rotation Z" , textRef.current.rotation.z)
 
       };
     
@@ -30,22 +34,13 @@ export default function Experience()
             <meshStandardMaterial color="gray" />
         </mesh>
 
-        {/* First Project */}
-        <group>
-            <mesh castShadow position-y={ 0.7 } position-x={ -2 } onClick={handleMeshClick} >
-                <boxGeometry/>
-                <meshStandardMaterial color="orange" />
-            </mesh>
-            <Text fontSize={0.3} position={[-2.3,1.6,0]}>First</Text>
-        </group>
         {/* Second Project */}
         <group>
             <mesh castShadow position={[7.5,3.5,-25]} onClick={handleMeshClick} rotation-y={ - Math.PI * 0.12 } scale-x={7.5} scale-y={4}>
                 <planeGeometry />
                 <meshStandardMaterial color="white" />
             </mesh>
-            <Text fontSize={2} rotation-x={ - Math.PI * 0.5 } position={[7.5,0.5,-25]} >Third</Text>
-
+                <Text ref={textRef} color="black" fontSize={2} rotation={[-1.5,-0.01, -0.39]} position={[7.5,0,-25]} >Third</Text>
         </group>
         <group>
             <mesh castShadow position={[-7.5,3.5,0]} onClick={handleMeshClick} rotation-y={ - Math.PI * -0.12 } scale-x={7.5} scale-y={4}>
--- src/Player.jsx
+++ src/Player.jsx
@@ -10,16 +10,24 @@ function Player({ newRotation   }) {
     const cameraRef = useRef();
     const cubeRef = useRef();
 
+    const cubee = useRef();
+
     const [rotation, setRotation] = useState([0, 0, 0]);
 
+    // Define a state to track whether overview click is on or off
+    const [overviewClickInProgress, setOverviewClickInProgress] = useState(false);
+    // Define a state to track camera Road on or off
+    const [cameraRoad, setCameraRoad] = useState(true)
 
     const handleWheel = (e) => {
       const delta = e.deltaY;
       const positionSpeed = 0.01; 
       // const rotSpeed = 0.001;
-      cameraRef.current.position.z += delta * positionSpeed;
-      console.log("Camera Position Z:", cameraRef.current.position.z);
-      setRotation([0,0,0]); // reset the rotation back to 0,0,0
+      if(cameraRoad){
+        cameraRef.current.position.z += delta * positionSpeed;
+        console.log("Camera Position Z:", cameraRef.current.position.z);
+      }
+      // setRotation([0,0,0]); // reset the rotation back to 0,0,0
 
 
   };
@@ -37,24 +45,35 @@ function Player({ newRotation   }) {
       };
     }, [newRotation]);
 
+  const handleCubeClick = () => {
+    setOverviewClickInProgress(true)
+    setCameraRoad(false)
+    cameraRef.current.position.set(0, 25, 18); // Adjust the Y value to set the height
+    // cameraRef.current.rotation.set(-Math.PI / 1, 0, 0); // Adjust the X value for the desired pitch
+  }
+
   useFrame(() => {
-      // const cubePosition = cubeRef.current.position
-      // const cubeRotation = cubeRef.current.rotation
+      const cubePosition = cubeRef.current.position
+      const cubeRotation = cubeRef.current.rotation
 
       // set Camera Position & Rotation to Cube Position & Rotation 
       // cameraRef.current.position.copy(cubePosition);
       // cameraRef.current.rotation.copy(cubeRotation);
 
-      // update cube rotation to new rotation (for click event)
-      cubeRef.current.rotation.x = rotation[0];
-      cubeRef.current.rotation.y = rotation[1];
-      cubeRef.current.rotation.z = rotation[2];
+      // update cube rotation to new rotation (for click event on Planes)
+      // cubeRef.current.rotation.x = rotation[0];
+      // cubeRef.current.rotation.y = rotation[1];
+      // cubeRef.current.rotation.z = rotation[2];
 
       // option withput Cube Player:
-      const cameraPosition = cameraRef.current.position 
+      // const cameraPosition = cameraRef.current.position
+      // const cameraRotation = cameraRef.current.rotation
+ 
       // Set the camera's rotation to look at itself
-      cameraRef.current.lookAt(cameraPosition);
-     
+      if (!overviewClickInProgress) {
+        const cameraPosition = cameraRef.current.position;
+        cameraRef.current.lookAt(cameraPosition);
+      }     
 
   });
     return (
@@ -62,11 +81,16 @@ function Player({ newRotation   }) {
       <>
       <OrbitControls enableZoom={false}/>
 
-      <PerspectiveCamera makeDefault ref={cameraRef} position={[0,2,1]} />
+      <PerspectiveCamera makeDefault ref={cameraRef} position={[0,2,0]}/>
       {/* Player */}
       <mesh ref={cubeRef} position={[0,2,1]} rotation-y={ - Math.PI * -12 } scale={ 0.4 }>
             <boxGeometry />
             <meshStandardMaterial color="hotpink" />
+      </mesh>  
+      {/* Click me to get bird eye perspective */}
+      <mesh ref={cubee} onClick={handleCubeClick} position={[0,2,-2]} rotation-y={ - Math.PI * -12 } scale={ 0.4 }>
+            <boxGeometry />
+            <meshStandardMaterial color="skyblue" />
       </mesh>  
     </>
 
