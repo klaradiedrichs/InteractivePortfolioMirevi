d7cccceb0e5e35fbb21b9c7ee2139f8924df05bf
klaradiedrichs <klara.diedrichs@study.hs-duesseldorf.de>
2024-06-12 23:29:08 +0200

    deleted old files

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,17 @@ import React, { useRef } from 'react';
 import * as THREE from 'three';
 import { useState , useEffect} from 'react'
 import './App.css'
-import {Canvas, useThree, useFrame} from "@react-three/fiber";
-import {KeyboardControls, Environment, PerspectiveCamera, ScrollControls, Preload, useGLTF} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {KeyboardControls, ScrollControls, Preload} from "@react-three/drei";
 import Overlay from './UI/Overlay';
-import { Perf } from 'r3f-perf';
 import Experience from './Road/Experience2';
-import WallExp from './Walls/WallsExp';
-import WDRScene from './wdr/WDRScene';
-import GenerationSpeaks from './wdr/GenerationSpeaks';
-import Fraktale from './Fraktale/Fraktale';
 import {useStore} from './stores/useStore';
 function App() {
 
-  const [isScene1Visible, setScene1Visible] = useState(true);
   const [backToStart, setBackToStart] = useState(false);
   const active = useStore((state) => state.active);
   const scrollBarColor = useStore((state) => state.scrollBarColor);
   const cameraRoad = useStore((state) => state.cameraRoad);
-
-  const handleStart = () => {
-    setBackToStart(true);
-  }
 
   useEffect(() => {
     const scrollbar = document.documentElement.style;
@@ -56,14 +46,9 @@ function App() {
         { name: 'rightward', keys: [ 'ArrowRight', 'KeyD' ] },
     ] }>
         <Canvas shadows>
-              {/* <Perf position='top-right'/> */}
               <ScrollControls enabled={active === null ? true : false} pages={10} damping={0.5 } >
                 <Experience setBackToStart={setBackToStart} backToStart={backToStart}/>
               </ScrollControls>
-              {/* <WallExp /> */}
-              {/* <WDRScene /> */}
-              {/* <Fraktale /> */}
-              {/* <GenerationSpeaks/> */}
               <Preload all />
 
         </Canvas>
